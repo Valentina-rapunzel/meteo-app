5039ccb06c4512547be353168e0ebe7fa60908f4
Valentina-rapunzel <111701493+Valentina-rapunzel@users.noreply.github.com>
2024-05-06 01:56:37 +0200

    Edited the search engine
    Added 2 functions to show the city and its temperature

--- src/app.js
+++ src/app.js
@@ -1,21 +1,31 @@
+function refreshWeather(response) {
+  let temperature = response.data.temperature.current;
+  console.log(temperature);
+  let temperatureElement = document.querySelector("#app-temperature-value");
+  temperatureElement.innerHTML = Math.round(temperature);
+  let city = response.data.city;
+  let country = response.data.country;
+  let location = `${city}, ${country}`;
+
+  let cityElement = document.querySelector("#app-city");
+  console.log(cityElement);
+  cityElement.innerHTML = location;
+}
+
 function searchCity(city) {
   // make an API call and change the interface
+  let key = "b30a2d9fef22b5o0t83182be74814ec8";
+  let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}&units=metric`;
+  axios.get(url).then(refreshWeather);
 }
 
 function showCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   console.log(searchInput.value);
-  let cityElement = document.querySelector("#app-city");
-  console.log(cityElement);
-  cityElement.innerHTML = searchInput.value;
-  searchCity = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 console.log(searchForm);
 searchForm.addEventListener("submit", showCity);
-
-let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}&units=metric`;
-let key = "b30a2d9fef22b5o0t83182be74814ec8";
-let city = searchInput.value;
