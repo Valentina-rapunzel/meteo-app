eb13991a3e310a38d9f6b1ddeb10318e0d869887
Valentina-rapunzel <111701493+Valentina-rapunzel@users.noreply.github.com>
2024-05-06 00:29:02 +0200

    Created a search engine
    Added ids to some form elements

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
+function searchCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  console.log(searchInput.value);
+  let searchCity = document.querySelector("#app-city");
+  console.log(searchCity);
+  searchCity.innerHTML = searchInput.value;
+}
 
+let searchForm = document.querySelector("#search-form");
+console.log(searchForm);
+searchForm.addEventListener("submit", searchCity);
